4e449cb349257f42ecd21694766d17e3448ddd61
alfahadarc <alfahadarc@gmail.com>
2021-02-26 07:41:51 +0600

    detecting git action

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAR0B8gHHGunkPi-QFLEhYoOT9SRqhaxys",
+    authDomain: "fastmed-c2511.firebaseapp.com",
+    projectId: "fastmed-c2511",
+    storageBucket: "fastmed-c2511.appspot.com",
+    messagingSenderId: "452216551733",
+    appId: "1:452216551733:web:a9461f3ec481d298d7347c",
+    measurementId: "G-TV79WY5G6H"
+  }
+
 };
